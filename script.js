9ee8c2f3b8b3a48d17c60b26076638528a7ecab1
m1104m <m1104m@gmail.com>
2020-05-26 11:21:26 +0900

    パラメーター追加

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function search()
 {
   var word = document.getElementById('word').value;
   var word_enc = encodeURIComponent(word);
-  var url = 'https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Ck%3A' + word_enc + '%2Cp_n_feature_nineteen_browse-bin%3A3169286051'
+  var url = 'https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Ck%3A' + word_enc + '%2Cp_n_feature_nineteen_browse-bin%3A3169286051&openExternalBrowser=1'
   window.open(url, '_blank');
 }
