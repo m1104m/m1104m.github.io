44e491d0729c8437606e746862d87b2fc0755b0e
m1104m <m1104m@gmail.com>
2020-06-10 17:23:18 +0900

    Enterの追加

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function search()
   var url = 'https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Ck%3A' + word_enc + '%2Cp_n_feature_nineteen_browse-bin%3A3169286051&openExternalBrowser=1'
   window.open(url, '_blank');
 }
+
+function enter(){
+  if( window.event.keyCode == 13 ){
+    document.form1.submit();
+  }
+}
